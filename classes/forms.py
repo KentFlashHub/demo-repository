36489b8f441e377f97ab5553d50040f5f33cff98
Daniel Write <daniel@writefamily.com>
2024-04-13 20:49:41 -0400

    added enrollment form

--- classes/forms.py
+++ classes/forms.py
@@ -1,3 +1,22 @@
 from django.forms import ModelForm
 from django import forms
 from django.contrib.auth.models import User
+from .models import Class
+
+class ClassForm(ModelForm):
+	class Meta:
+		model = Class
+		fields = ['name', 'prefixed_id']
+
+class ClassNameForm(forms.ModelForm):
+    class Meta:
+        model = Class
+        fields = ['name']
+
+    name = forms.ModelChoiceField(
+        queryset=Class.objects.all(),
+        to_field_name='name',
+        required=True,  
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+    
--- classes/views.py
+++ classes/views.py
@@ -4,6 +4,15 @@ import csv
 from django.contrib import messages
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
+from .forms import ClassNameForm
+from .models import Class
 
+@login_required
 def enroll(request):
-    return render(request, 'classes/base.html')
+    if request.method == 'POST':
+        form = ClassForm(request.POST)
+        return b'hry'
+    else:
+        classes = Class.objects.all();
+        context = {'classes': classes, 'form': ClassNameForm}
+        return render(request, 'classes/enroll.html', context)
